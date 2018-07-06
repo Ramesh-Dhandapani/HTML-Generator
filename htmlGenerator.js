b013ab195899dddd11062fdf6b1820e1486f1bd0
ramesh-dhandapani <ramesh.dhandapani@thomascook.com>
2018-07-06 06:40:38 +0530

    HTML Generator v1.0
    Multiple  HTML Files will be Generate based on HTML Template, Data excel,Mapping excel and Iterate Template Excel(Optional).

---
+++ htmlGenerator.js
@@ -0,0 +1,177 @@
+/**
+ * Generate Data Json and Mapping Json files based on corresponding xlsx files.
+ * AND
+ * Generate HTML files based on HTML Template file, Data json and Mapping Json
+ */
+
+(function () {
+    /** importing support lib */
+    var XLSX = require('xlsx');
+    var fs = require('fs');
+    var cheerio = require('cheerio');
+
+    var dataJsonArrObj, mappingJson;
+    var xlData, linksData;
+
+    generateContentJsonFile = function (srcFilePath, distFilePath, hasLink) {
+        var workbook = XLSX.readFile(srcFilePath);
+        var sheet_name_list = workbook.SheetNames;
+        xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+        if (hasLink)
+            alterXData();
+        fs.writeFileSync(distFilePath, JSON.stringify(xlData));
+    }
+    alterXData = function () {
+        console.log("----alterData----");
+        for (linkData of linksData) {
+            let keyValArry = linkData["TEMP_MAP_KEY"].split(',');
+            console.log(keyValArry);
+            changeXData(linkData["NEW_KEY"], keyValArry, linkData['TEMPLATE'], linkData['LAST_TEMPLATE']);
+        }
+    }
+    generateLinkJsonFile = function (srcFilePath, distFilePath) {
+        var workbook = XLSX.readFile(srcFilePath);
+        var sheet_name_list = workbook.SheetNames;
+        linksData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+        fs.writeFileSync(distFilePath, JSON.stringify(linksData));
+    }
+    changeXData = function (newKey, keyValArry, templateContent, lastTemplate) {
+        for (var contObj of xlData) {
+
+            let ulContent = '';
+            let tempPlaceHolderVal = [];
+            for (var keyval of keyValArry) {
+                var key_val = keyval.split(':');
+                tempPlaceHolderVal[key_val[0]] = formatToArray(contObj[key_val[1]]);
+            }
+            let tempPlaceHolderKeys = Object.keys(tempPlaceHolderVal);
+            var i = 0;
+            var isOuterBreak = false;
+            while (true) {
+                var temp = templateContent;
+                for (let j = 0; j < tempPlaceHolderKeys.length; j++) {
+                    let valArr = tempPlaceHolderVal[tempPlaceHolderKeys[j]];
+                    console.log("place holder and text");
+                    if (valArr[i]) {
+                        console.log(tempPlaceHolderKeys[j] + " --- " + valArr[i]);
+                        var palceHolder = new RegExp(tempPlaceHolderKeys[j], "g");
+                        temp = temp.replace(palceHolder, valArr[i]);
+                        console.log(temp);
+                    } else {
+                        if (tempPlaceHolderKeys[j] == "#LINK") {
+                            temp = lastTemplate;
+                        } else if (j == tempPlaceHolderKeys.length - 1) {
+                            temp='';
+                            isOuterBreak = true;
+                            break;
+                        }else{
+                            console.log(tempPlaceHolderKeys[j] + " --- " + valArr[i]);
+                            var palceHolder = new RegExp(tempPlaceHolderKeys[j], "g");
+                            temp = temp.replace(palceHolder, '');
+                            console.log(temp);
+                        }
+                    }
+                }
+                console.log("--- temp ---");
+                console.log(temp);
+                ulContent += temp;
+                if (isOuterBreak) break;
+                i++;
+            }
+            console.log("-- ulContent --");
+            console.log(ulContent);
+            // let value = formatToArray(contObj[valueKey]); 
+            // let link =  formatToArray(contObj[linkKey]);
+            // let ulContent = '';
+            // let linkCount;
+            // linkCount = (lastTemplate == undefined) ? value.length : value.length - 1;
+            // for (let i = 0; i < linkCount; i++) {
+            //     ulContent += templateContent.replace(/#LINK/g, link[i]).replace(/#TEXT/g, value[i]);
+            // }
+            // if (lastTemplate) {
+            //     ulContent += lastTemplate.replace(/#LINK/g, link[value.length - 1]).replace(/#TEXT/g, value[value.length - 1]);
+            // }
+            contObj[newKey] = ulContent;
+        }
+    }
+    generateMappingJsonFile = function (srcFilePath, distFilePath) {
+        var config_workbook = XLSX.readFile(srcFilePath);
+        var config_sheet_name_list = config_workbook.SheetNames;
+        var configData = XLSX.utils.sheet_to_json(config_workbook.Sheets[config_sheet_name_list[0]]);
+        var configDataJson = traverseObject(configData);
+        fs.writeFileSync(distFilePath, JSON.stringify(configDataJson));
+    }
+
+    traverseObject = function (configData) {
+        let obj = {}
+        for (var elem of configData) {
+            obj[elem.Name] = elem;
+        }
+        return obj;
+    }
+
+    formatToArray = function (txt) {
+        let value = [];
+        console.log(txt);
+        if (txt) {
+            value = txt.replace(/\r|\n/g, '');
+            value = value.split(";");
+            if (value[value.length - 1] == "") value.pop();
+        }
+        return value;
+    }
+
+    generateHtmlFile = function (distDir, htmlTemplateFile, outputDataJsonFilePath, mappingJsonFilePath) {
+        if (!fs.existsSync(distDir)) {
+            fs.mkdirSync(distDir);
+        }
+        setTimeout(function () {
+            dataJsonArrObj = require("./" + outputDataJsonFilePath);
+            mappingJson = require("./" + mappingJsonFilePath);
+            let i = 1;
+            for (let dataJson of dataJsonArrObj) {
+                //  console.log(dataJson);
+                //  console.log(dataJson['Meta_Title']);
+                var outPath = distDir + '//' + dataJson['Meta_Title'].split('|')[0].replace(/ /g, '') + '.html';
+                //   console.log(outPath)
+                genHtmlFile(htmlTemplateFile, dataJson, mappingJson, outPath);
+            }
+        }, 1000);
+    }
+
+    genHtmlFile = function (htmlTemplateFile, dataJson, mappingJson, fileName) {
+        fs.readFile(htmlTemplateFile, { encoding: 'utf8' }, function (error, data) {
+            var $ = cheerio.load(data); // load in the HTML into cheerio
+            for (var key in dataJson) {
+                setContentToPlaceHolder($, key, mappingJson, dataJson);
+            }
+            fs.writeFile(fileName, $.html());
+        });
+    }
+
+    setContentToPlaceHolder = function ($, key, mappingJson, dataJson) {
+        let mapObj = mappingJson[key];
+        if (mapObj) {
+            let attr = mapObj['Attribute'];
+            let id = mapObj['Element_ID'];
+            let attrVal = dataJson[key];
+            if (attr == 'innerHtml') {
+                $('#' + id).html(attrVal);
+            } else if (attr == 'src' || attr == 'content' || attr == 'alt' || attr == 'href' || attr == 'mUrl') {
+                $('#' + id).attr(attr, attrVal);
+            } else if (attr == 'value') {
+                $('#' + id).val(attrVal);
+            } else if (attr === "keyValue") {
+                let keyVal = attrVal.split("=");
+                $('#' + id).attr(keyVal[0].trim(), keyVal[1].trim());
+            } else {
+                console.log(key+" ---- "+attr+ "Attribute Type Not defined "+id)
+            }
+        }
+    }
+
+    replaceContent = function (str) {
+        return str.replace(/</g, "").replace(/>/g, "");
+    }
+
+})();
---
+++ server.js
@@ -0,0 +1,81 @@
+﻿var Express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+require('./htmlGenerator.js');
+var app = Express();
+app.use(bodyParser.json());
+var templateName;
+var srcDir;
+var distDir;
+var market;
+var hasLink=false;
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        templateName = req.body.templateName;
+        market = req.body.market;
+        console.log(market);
+        srcDir = './src/' +req.body.market+"/"+ req.body.templateName;
+        if (!fs.existsSync(srcDir)) {
+            fs.mkdirSync(srcDir);
+        }    
+        callback(null, srcDir);
+    },
+    filename: function (req, file, callback) {
+        let fileName = req.body.templateName;
+        if (file.fieldname === 'dataInputFile') fileName += "_data.xlsx";
+        else if (file.fieldname === 'mappingInputFile') fileName += "_mapping.xlsx";
+        else if (file.fieldname === 'linkInputFile'){
+            fileName += "_link.xlsx";
+        }
+        else if (file.fieldname === 'templateInputFile') fileName += "_template.html";
+        callback(null, fileName);
+    }
+});
+
+var upload = multer({ storage: Storage }).fields([{
+    name: 'dataInputFile', maxCount: 1
+}, {
+    name: 'mappingInputFile', maxCount: 1
+}, {
+    name: 'linkInputFile', maxCount: 1
+}, {
+    name: 'templateInputFile', maxCount: 1
+}]);
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/api/Upload", function (req, res, next) {
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err);
+            return res.end("Something went wrong!");
+        }
+
+        let linkXlsFilePath = srcDir+'\\'+templateName+'_link.xlsx';
+        let linkJsonFilePath = srcDir+'\\'+templateName+'_link.json';
+        if (fs.existsSync(linkXlsFilePath)) {
+            generateLinkJsonFile(linkXlsFilePath, linkJsonFilePath);
+            hasLink=true;
+        }    
+        let inputDataXlsFilePath = srcDir+'\\'+templateName+'_data.xlsx';
+        let outputDataJsonFilePath = srcDir+'\\'+templateName+'_data.json';
+        generateContentJsonFile(inputDataXlsFilePath,outputDataJsonFilePath,hasLink);
+
+        let mappingXlsFilePath = srcDir+'\\'+templateName+'_mapping.xlsx';
+        let mappingJsonFilePath = srcDir+'\\'+templateName+'_mapping.json';
+        generateMappingJsonFile(mappingXlsFilePath, mappingJsonFilePath);
+       
+        var htmlTemplateFile = srcDir+'\\'+templateName+'_template.html';
+        distDir = '..//tc-exp-landing-pages/src/OutputPages/'+market+'/'+ req.body.templateName;
+        generateHtmlFile(distDir,htmlTemplateFile,outputDataJsonFilePath,mappingJsonFilePath);
+
+        return res.end("HTML Files generate sucessfully!.");
+    });
+});
+
+app.listen(2000, function (a) {
+    console.log("Listening to port 2000");
+});
